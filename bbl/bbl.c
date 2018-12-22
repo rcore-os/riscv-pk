11fe816ca3b7745268e9322c51cd115d6059654e
WangRunji <wangrunji0408@163.com>
2018-12-22 15:37:19 +0800

    setup recursive page table before entering S-Mode for both 32/64

--- bbl/bbl.c
+++ bbl/bbl.c
@@ -40,6 +40,38 @@ static void filter_dtb(uintptr_t source)
   filter_compat(dest, "riscv,debug-013");
 }
 
+// static alloc root page table
+static pte_t page_table[1 << RISCV_PGLEVEL_BITS] __attribute__((aligned(RISCV_PGSIZE)));
+
+static void setup_page_table32()
+{
+  // map kernel [0x200..] 0x80000000..
+  int i_end = dtb_output() / MEGAPAGE_SIZE;
+  for(int i=0x200; i<i_end; ++i) {
+    page_table[i] = pte_create(i << RISCV_PGLEVEL_BITS, PTE_R | PTE_W | PTE_X);    
+  }
+  // map recursive [0x3fe] (V), [0x3ff] (VRW)
+  uintptr_t page_table_ppn = (uintptr_t)page_table >> RISCV_PGSHIFT;
+  page_table[0x3fe] = pte_create(page_table_ppn, 0);
+  page_table[0x3ff] = pte_create(page_table_ppn, PTE_R | PTE_W);
+}
+
+static void setup_page_table64()
+{
+  // map kernel [0o776] 0x80000000 -> 0xffffffff_80000000 (size = 1G)
+  page_table[0776] = pte_create(0x80000, PTE_R | PTE_W | PTE_X);
+  // map recursive [0o774] (V), [0o775] (VRW)
+  uintptr_t page_table_ppn = (uintptr_t)page_table >> RISCV_PGSHIFT;
+  page_table[0774] = pte_create(page_table_ppn, 0);
+  page_table[0775] = pte_create(page_table_ppn, PTE_R | PTE_W);
+}
+
+static void enable_paging() { 
+  uintptr_t page_table_ppn = (uintptr_t)page_table >> RISCV_PGSHIFT;
+  write_csr(sptbr, page_table_ppn | SATP_MODE_CHOICE);
+  flush_tlb();
+}
+
 void boot_other_hart(uintptr_t unused __attribute__((unused)))
 {
   const void* entry;
@@ -69,6 +101,7 @@ void boot_other_hart(uintptr_t unused __attribute__((unused)))
   extern void* bbl_functions;
   enter_machine_mode(entry, hartid, dtb_output(), ~disabled_hart_mask & hart_mask, (uintptr_t)&bbl_functions);
 #else /* Run bbl in supervisor mode */
+  enable_paging();
   enter_supervisor_mode(entry, hartid, dtb_output(), ~disabled_hart_mask & hart_mask);
 #endif
 }
@@ -85,5 +118,13 @@ void boot_loader(uintptr_t dtb)
   mb();
   /* Use optional FDT preloaded external payload if present */
   entry_point = kernel_start ? kernel_start : &_payload_start;
+#ifndef BBL_BOOT_MACHINE
+#if __riscv_xlen == 64
+  setup_page_table64();
+  entry_point += 0xffffffff00000000;
+#else
+  setup_page_table32();
+#endif
+#endif
   boot_other_hart(0);
 }
