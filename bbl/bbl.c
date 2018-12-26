f4ec45a908005331149af5816247dcb89fbd4654
dzy <daizy15@mails.tsinghua.edu.cn>
2018-12-26 09:52:18 +0800

    support sv48 in bbl

--- bbl/bbl.c
+++ bbl/bbl.c
@@ -41,34 +41,49 @@ static void filter_dtb(uintptr_t source)
 }
 
 // static alloc root page table
-static pte_t page_table[1 << RISCV_PGLEVEL_BITS] __attribute__((aligned(RISCV_PGSIZE)));
+static pte_t root_table[1 << RISCV_PGLEVEL_BITS] __attribute__((aligned(RISCV_PGSIZE)));
 
-static void setup_page_table32()
+// only used for Sv48 systems, to map 0xFFFF_FFFF_8000_0000 to 0x8000_0000.
+static pte_t p3_table[1 << RISCV_PGLEVEL_BITS] __attribute__((aligned(RISCV_PGSIZE)));
+
+static void setup_page_table_sv32()
 {
   // map kernel [0x200..] 0x80000000..
   int i_end = dtb_output() / MEGAPAGE_SIZE;
   for(int i=0x200; i<i_end; ++i) {
-    page_table[i] = pte_create(i << RISCV_PGLEVEL_BITS, PTE_R | PTE_W | PTE_X);    
+    root_table[i] = pte_create(i << RISCV_PGLEVEL_BITS, PTE_R | PTE_W | PTE_X);
   }
   // map recursive [0x3fe] (V), [0x3ff] (VRW)
-  uintptr_t page_table_ppn = (uintptr_t)page_table >> RISCV_PGSHIFT;
-  page_table[0x3fe] = pte_create(page_table_ppn, 0);
-  page_table[0x3ff] = pte_create(page_table_ppn, PTE_R | PTE_W);
+  uintptr_t root_table_ppn = (uintptr_t)root_table >> RISCV_PGSHIFT;
+  root_table[0x3fe] = pte_create(root_table_ppn, 0);
+  root_table[0x3ff] = pte_create(root_table_ppn, PTE_R | PTE_W);
 }
 
-static void setup_page_table64()
+static void setup_page_table_sv39()
 {
   // map kernel [0o776] 0x80000000 -> 0xffffffff_80000000 (size = 1G)
-  page_table[0776] = pte_create(0x80000, PTE_R | PTE_W | PTE_X);
+  root_table[0776] = pte_create(0x80000, PTE_R | PTE_W | PTE_X);
   // map recursive [0o774] (V), [0o775] (VRW)
-  uintptr_t page_table_ppn = (uintptr_t)page_table >> RISCV_PGSHIFT;
-  page_table[0774] = pte_create(page_table_ppn, 0);
-  page_table[0775] = pte_create(page_table_ppn, PTE_R | PTE_W);
+  uintptr_t root_table_ppn = (uintptr_t)root_table >> RISCV_PGSHIFT;
+  root_table[0774] = pte_create(root_table_ppn, 0);
+  root_table[0775] = pte_create(root_table_ppn, PTE_R | PTE_W);
 }
 
-static void enable_paging() { 
-  uintptr_t page_table_ppn = (uintptr_t)page_table >> RISCV_PGSHIFT;
-  write_csr(sptbr, page_table_ppn | SATP_MODE_CHOICE);
+static void setup_page_table_sv48()
+{
+	// map kernel va FFFF_FFFF_8000_0000 -> pa 0000_0000_8000_0000
+	uintptr_t p3_table_ppn = (uintptr_t) p3_table >> RISCV_PGSHIFT;
+  root_table[0777] = pte_create(p3_table_ppn, 0);
+	p3_table[0776] = pte_create(0x80000, PTE_R | PTE_W | PTE_X);
+
+  uintptr_t root_table_ppn = (uintptr_t) root_table >> RISCV_PGSHIFT;
+  root_table[0775] = pte_create(root_table_ppn, 0);
+  root_table[0776] = pte_create(root_table_ppn, PTE_R | PTE_W);
+}
+
+static void enable_paging() {
+  uintptr_t root_table_ppn = (uintptr_t)root_table >> RISCV_PGSHIFT;
+  write_csr(sptbr, root_table_ppn | SATP_MODE_CHOICE);
   flush_tlb();
 }
 
@@ -120,10 +135,12 @@ void boot_loader(uintptr_t dtb)
   entry_point = kernel_start ? kernel_start : &_payload_start;
 #ifndef BBL_BOOT_MACHINE
 #if __riscv_xlen == 64
-  setup_page_table64();
+  setup_page_table_sv48();
+// XXX: hack
+//  setup_page_table_sv39();
   entry_point += 0xffffffff00000000;
 #else
-  setup_page_table32();
+  setup_page_table_sv32();
 #endif
 #endif
   boot_other_hart(0);
